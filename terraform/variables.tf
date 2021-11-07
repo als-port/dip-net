467fc5e73587767a93d7e0b2cc7ba387406d364b
alsxs <oh@hotbox.ru>
2021-11-07 15:19:47 +0300

    worked by resource eks 07.11.21

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "serv_sert" {
+  default = "arn:aws:acm:eu-central-1:336639867151:certificate/4618aba8-3330-45f1-9467-2be73fe6a222"
+}
+
+variable "sert" {
+  default = "arn:aws:acm:eu-central-1:336639867151:certificate/604c31d3-1bfb-490d-955e-88b008e50118"
+}
+
+variable "cluster_name" {
+  default = "dip-net-cluster"
+}
